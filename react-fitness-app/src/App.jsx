fb60c1f0644b1de7091aeeb641f3eaece61571ad
jennyo <jennifer.oleary.10@gmail.com>
2025-05-20 23:30:52 -0500

    begin dashboard

--- react-fitness-app/src/App.jsx
+++ react-fitness-app/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Dashboard from './components/dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Dashboard />
     </>
   )
 }
